fe3516c5153c1dc3dd2affe2e27c0faf87ca9947
Heniloza <ozahenil73@gmail.com>
2025-06-17 22:15:54 +0530

    feat : Like and comment and delete post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
 import CreatePost from "@/components/CreatePost";
+import PostCard from "@/components/PostCard";
 import RecommendedUsers from "@/components/RecommendedUsers";
 import { currentUser } from "@clerk/nextjs/server";
 
 async function Home() {
   const user = await currentUser();
   const posts = await getPosts();
+  const dbUserId = await getDbUserId();
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
       <div className="space-y-6">
-        {posts.map((post) => (
-          <PostCard key={post.id} post={post} />
+        {posts?.map((post) => (
+          <PostCard key={post.id} post={post} dbUserId={dbUserId} />
         ))}
       </div>
       <div className="lg:col-span-4 lg:block hidden sticky top-20">
